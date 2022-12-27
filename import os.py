d2e10473269275cce58c006080bc142d12ab4f3e
wilylejardinier <franck1.asselin@sfr.fr>
2022-12-27 17:30:47 +0100

    on peut maintenant ecrire le format voulut exemple: {artist} - {title}

--- import os.py
+++ import os.py
@@ -7,7 +7,7 @@ from mutagen.dsf import DSF
 import tkinter as tk
 from tkinter import filedialog
 
-def rename_folder(folder_path):
+def rename_folder(folder_path, name_format):
   # Get a list of all the files in the directory
   file_list = os.listdir(folder_path)
   
@@ -34,14 +34,14 @@ def rename_folder(folder_path):
     title = audio['title'][0]
     artist = audio['artist'][0]
   
-  # Construct the new name for the folder
-  new_name = artist + ' - ' + title
+  # Construct the new name for the folder using the specified format
+  new_name = name_format.format(artist=artist, title=title)
   
   # Add a number in parentheses to the end of the name if it already exists
   counter = 0
   while os.path.exists(os.path.join(os.path.dirname(folder_path), new_name)):
     counter += 1
-    new_name = artist + ' - ' + title + ' (' + str(counter) + ')'
+    new_name = name_format.format(artist=artist, title=title) + ' (' + str(counter) + ')'
   
   # Rename the folder
   os.rename(folder_path, os.path.join(os.path.dirname(folder_path), new_name))
@@ -52,6 +52,10 @@ window = tk.Tk()
 # Make the window movable
 window.resizable(True, True)
 
+# Create a text field for the user to enter the folder name format
+name_format_field = tk.Entry(window)
+name_format_field.pack()
+
 # Create a function to open the file browser and get the selected directories
 def select_directories():
   directories = filedialog.askdirectory(parent=window, title='Select directories to rename')
@@ -59,9 +63,12 @@ def select_directories():
     # Split the directories into a list of individual directories
     directories = directories.split(';')
     
+    # Get the folder name format entered by the user
+    name_format = name_format_field.get()
+    
     # Rename each directory
     for directory in directories:
-      rename_folder(directory)
+      rename_folder(directory, name_format)
 
 # Create a button to open the file browser
 button = tk.Button(text='Select directories', command=select_directories)
@@ -69,3 +76,5 @@ button.pack()
 
 # Run the main loop
 window.mainloop()
+
+   
